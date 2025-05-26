6537330308433fdaad0b5a4854cc95c63ccc9120
torcanhack <tor409617@gmail.com>
2025-05-26 07:40:47 +0100

    defining crud operations

---
+++ server/crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from models import User, Comment, BlogPost
+from schemas import UserCreate, BlogPostCreate, BlogPostUpdate, CommentCreate, CommentUpdate
+from auth import get_password_hash
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+
